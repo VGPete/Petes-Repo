e0e1cd656fb1f33ef99b6bfe542507c358d73434
VGPete <peterjshumway@gmail.com>
2022-01-06 23:18:22 -0700

    Working Links
    Links are now showing correctly in the list.

--- js/main.js
+++ js/main.js
@@ -14,13 +14,7 @@ function addLink() {
     for(var i = 0; i < links.length; i++) {
     	var label = links[i].label;
       var url = links[i].url;
-    	var link = "<a href=\"" + url + ">\"" + label + "\"</a>";
-      const ol = document.querySelector("#directoryOL");
-      const li = document.createElement('li');
-      var text = document.createTextNode(link);
-      li.appendChild(text);
-      ol.appendChild(li);
+    	var link = "<a href=\"" + url + "\">" + label + "</a>";
+      $("#directoryOL").append( '<li>' + link + '</li>' )
 		}
-
-
 }
