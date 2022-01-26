020a31f2385baebfdc797a123e95ffeb22c38883
VGPete <peterjshumway@gmail.com>
2022-01-26 15:11:46 -0700

    Week 4 Update
    Added a Home button to return back to the main menu in the site.
    Updated Quiz Ninja with chapter 8-12 exercises
    Added week 4 notes & link to our team tic tac toe game.

--- js/main.js
+++ js/main.js
@@ -15,6 +15,10 @@ function addLink() {
       {
         label:"Week3 Notes",
         url:"week3/index.html"
+      },
+      {
+        label:"Week4 Notes",
+        url:"week4/index.html"
       }
 
     ]
